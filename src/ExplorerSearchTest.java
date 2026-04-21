f01ee984e4d2fd3985865dda404c4b01a8425965
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-04-20 22:14:01 -0700

    added more tests

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -15,6 +15,53 @@ public class ExplorerSearchTest {
         assertEquals(14, actual);
     }
 
+    @Test
+    public void testReachableArea_mostUnreachable() {
+        int[][] island = {
+            {2,2,3,3,3,3},
+            {3,2,3,1,3,1},
+            {3,1,3,0,3,3},
+            {3,1,2,2,3,1},
+            {1,3,3,2,3,2},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(2, actual);
+    }
     // Add more tests here!
     // Come up with varied cases
+    @Test
+    public void testExplorerLocation_topLeftCorner(){
+        int[][] island = {
+            {0,1,1,2},
+            {1,1,1,3},
+            {1,1,1,1}
+        };
+        int[] expected = {0,0};
+        assertArrayEquals(expected,ExplorerSearch.explorerLocation(island));
+    }
+
+        @Test
+    public void testExplorerLocation_middle(){
+        int[][] island = {
+            {1,1,1,2,2},
+            {1,1,1,3,1},
+            {1,1,0,1,1},
+            {1,1,1,1,1}
+        };
+        int[] expected = {2,2};
+        assertArrayEquals(expected,ExplorerSearch.explorerLocation(island));
+    }
+
+       @Test
+    public void testExplorerLocation_notFound_throwsException() {
+        int[][] island = {
+            {3, 2, 2,3},
+            {3, 2, 2,3},
+            {3, 2, 2,3}
+        };
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ExplorerSearch.explorerLocation(island);
+        });
+        assertEquals("No land area to explore", exception.getMessage());
+    }
 }
