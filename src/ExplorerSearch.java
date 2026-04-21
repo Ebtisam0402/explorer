9542f21b85b2e3c95c9ec093d73d6e102385f5b2
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-04-20 21:48:09 -0700

    implemented explorerSearch

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -32,6 +32,82 @@ public class ExplorerSearch {
         // Implement your method here!
         // Please also make more test cases
         // I STRONGLY RECOMMEND testing some helpers you might make too
-        return -1;
+        int[] startLocation = explorerLocation(island);
+        boolean[][] visited = new boolean[island.length][island[0].length];
+        return reachableArea(startLocation, island, visited);
+
+    }
+
+    private static int reachableArea(int[] currentLoc, int[][] island, boolean[][] visited){
+
+        int curR = currentLoc[0];
+        int curC = currentLoc[1];
+
+        if(visited[curR][curC]){
+            return 0;
+        }
+
+        visited[curR][curC]= true;
+
+        int count = 1;
+
+        for (int[] move: possibleMoves(island, currentLoc)){
+            count += reachableArea(move, island, visited);
+        }
+        return count;
+    }
+
+    public static List<int[]> possibleMoves(int[][] island, int[] location){
+        int curR = location[0];
+        int curC = location[1];
+
+        List<int[]> validLocs = new ArrayList<>();
+
+
+           //up
+        int newR = curR - 1;
+        int newC = curC;
+
+        if(newR >= 0 &&  island[newR][newC] != 2 && island[newR][newC] != 3){
+            validLocs.add(new int[] {newR, newC});
+        }
+
+        //down
+        newR = curR + 1;
+        newC = curC;
+
+        if(newR < island.length && island[newR][newC] != 2 && island[newR][newC] != 3){
+            validLocs.add(new int[] {newR, newC});
+        }
+
+        //Right
+        newR = curR;
+        newC = curC + 1;
+
+        if(newC < island[0].length && island[newR][newC] != 2 && island[newR][newC] != 3){
+            validLocs.add(new int[] {newR, newC});
+        }
+
+        //Left
+        newR = curR;
+        newC = curC -1;
+
+        if(newC >= 0 && island[newR][newC] != 2 && island[newR][newC] != 3){
+            validLocs.add(new int[] {newR, newC});
+        }
+
+        return validLocs;
+
+    }
+
+    public static int[] explorerLocation(int[][] island){
+        for(int row = 0; row < island.length; row++){
+            for(int col = 0; col < island[0].length; col++){
+                if(island[row][col] == 0){
+                    return new int []{row, col};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No land area to explore");
     }
 }
